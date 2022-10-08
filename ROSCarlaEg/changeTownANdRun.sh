9faeaa31573748b318dd57aa7e3d56d3f4d4ed83
AlexanderInUM <alexander.um.edu@gmail.com>
2022-10-07 21:13:41 -0700

    v1.0

---
+++ ROSCarlaEg/changeTownANdRun.sh
@@ -0,0 +1,3 @@
+source /home/alexander/carla-ros-bridge/catkin_ws/devel/setup.bash 
+roslaunch carla_ros_bridge carla_ros_bridge_with_example_ego_vehicle.launch town:=Town03 host:=172.19.240.1
+#rosrun rviz rviz
